a3f7b50db83de62ee06e20ea7736ac9a760830e7
Erhahon Imafidon <mafiosco4@gmail.com>
2023-08-16 00:20:37 +0100

    committing Imafidon's end

--- resources/js/Components/HeaderSection/index.tsx
+++ resources/js/Components/HeaderSection/index.tsx
@@ -1,4 +1,4 @@
-import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 // @ts-ignore
 import styles from "./styles.module.scss";
 import * as React from "react";
@@ -9,31 +9,50 @@ import searchSvg from "/public/assets/svg/search.svg";
 
 export default function () {
     return (
-      <section className={styles.homeSection}>
-          <div className={styles.videoContainer}>
-              <video poster="/assets/videos/bg/hero-video.mp4" playsInline autoPlay muted loop>
-                  <source src="/assets/videos/bg/hero-video.mp4" type="video/mp4"/>
-              </video>
-          </div>
-          <div className={styles.overlay}/>
-          <div className={styles.headerInfo}>
-              <h3>
-                  Welcome to a New <br/>
-                  Era of <b>Smart Living</b>.
-              </h3>
+        <section className={styles.homeSection}>
+            <div className={styles.videoContainer}>
+                <video
+                    poster="/assets/videos/bg/hero-video.mp4"
+                    playsInline
+                    autoPlay
+                    muted
+                    loop
+                >
+                    <source
+                        src="/assets/videos/bg/hero-video.mp4"
+                        type="video/mp4"
+                    />
+                </video>
+            </div>
+            <div className={styles.overlay} />
+            <div className={styles.headerInfo}>
+                <h3>
+                    Welcome to a New <br />
+                    Era of <b>Smart Living</b>.
+                </h3>
 
-              <p> Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet
-                  <br/>
-                  Mahiga Imporum  Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet</p>
+                <p>
+                    {" "}
+                    Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet
+                    <br />
+                    Mahiga Imporum Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor
+                    Sit Amet
+                </p>
 
-              <div className={styles.inputContainer}>
-                  <input placeholder={'Explore Cosgrove homes'}/>
-                  <img  className={styles.homeSvg} src={homeSvg} alt="Your SVG" />
-                  <img className={styles.searchSvg} src={searchSvg} alt="Your SVG" />
-              </div>
-          </div>
-
-
-      </section>
+                <div className={styles.inputContainer}>
+                    <input placeholder={"Explore Cosgrove homes"} />
+                    <img
+                        className={styles.homeSvg}
+                        src={homeSvg}
+                        alt="Your SVG"
+                    />
+                    <img
+                        className={styles.searchSvg}
+                        src={searchSvg}
+                        alt="Your SVG"
+                    />
+                </div>
+            </div>
+        </section>
     );
 }
